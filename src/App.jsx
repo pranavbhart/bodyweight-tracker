1c9988b085e2db7650bcf94023a4a6cb66665c2e
Pranav Bhart <pranav.bhart@bspoke.no>
2022-12-29 14:49:30 +0100

    feat: handle when date is the same as previous

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,27 @@ function App() {
   const [weightValue, setWeightValue] = useState(0);
   const [weightsArray, pushWeightToState] = useState([]);
 
-  const onButtonClick = () => {
+  const handleAdd = () => {
     if (date === '' || weightValue === 0) {
       alert("Date or weight is empty");
       return;
     }
-    pushWeightToState([...weightsArray, { date, weight: weightValue }]);
+    const foundIndex = weightsArray.findIndex((item) => item.date === date);
+    if (foundIndex !== -1) {
+      alert("Overwriting existing weight");
+      const newWeightsArray = [...weightsArray];
+      newWeightsArray[foundIndex] = {date, weight: weightValue};
+      pushWeightToState(newWeightsArray);
+    }
+    else {
+      pushWeightToState([...weightsArray, {date, weight: weightValue}]
+        .sort((a, b) => new Date(a.date) - new Date(b.date)));
+    }
+  }
+
+  const handleDelete = (index) => {
+    const newArray = weightsArray.filter((item, i) => i !== index);
+    pushWeightToState(newArray);
   }
 
   return (
@@ -26,10 +41,22 @@ function App() {
       <input type="date" value={date} onChange={e => setDate(e.target.value)}/>
       <p>weight</p>
       <input type="number" value={weightValue} onChange={e => setWeightValue(e.target.value)} />
-      <button onClick={onButtonClick}>add</button>
+      <button onClick={handleAdd}>add</button>
       <ul>
           {weightsArray.map((weightItem, index) => (
-              <li key={index}>{weightItem.date} - {weightItem.weight}</li>
+            <div key={index} className='weightInfo'>
+              <li >date: {weightItem.date} - {weightItem.weight} kg
+                <button onClick={() => handleDelete(index)} className='deleteButton' style={{
+                  backgroundColor: '#f1356d',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '0.5em',
+                  padding: '0.5em 1em',
+                  marginLeft: '1em',
+                  fontSize: '0.8em',
+                }}>delete</button>
+              </li>
+            </div>
           ))}
       </ul>
     </div>
