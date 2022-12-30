45faf73d3a7949855cb94a3ea5edcc6e2c0fdba5
pranavbhart <pranavbhart@gmail.com>
2022-12-30 11:39:28 +0100

    feat: use variables to save colors instead of hard-coding

--- src/MyChartComponent.jsx
+++ src/MyChartComponent.jsx
@@ -23,26 +23,38 @@ ChartJS.register(
 const MyChartComponent = ({ data }) => {
     console.log("MyChartComponent rendered");
     console.log("Chart data", data);
+    const cyanColor = {
+        strong: 'rgba(75,192,192,1)',
+        light: 'rgba(75,192,192,0.4)',
+    };
+    const offWhiteColor = {
+        default: 'rgba(220,220,220,1)'
+    }
+    const salmonColor = {
+        strong: 'rgba(255,99,132,1)',
+        light: 'rgba(255,99,132,0.4)',
+    }
+
     const chartData = {
         labels: data.map(item => item.date), // use the dates as the labels
         datasets: [
             {
-                label: 'My Data',
+                label: 'body weight',
                 data: data.map(item => item.weight), // use the numbers as the data points
                 fill: false,
                 lineTension: 0.1,
-                backgroundColor: 'rgba(75,192,192,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
+                backgroundColor: cyanColor.light,
+                borderColor: cyanColor.strong,
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
                 borderJoinStyle: 'miter',
-                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBorderColor: cyanColor.strong,
                 pointBackgroundColor: '#fff',
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
-                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBackgroundColor: cyanColor.strong,
+                pointHoverBorderColor: offWhiteColor.default,
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
